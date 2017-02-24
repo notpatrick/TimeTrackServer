6311e7a7919eaf1bcb656fd9ffc90fca52e421f2
Patrick <notpatrick@users.noreply.github.com>
2017-02-25 00:50:38 +0100

    Add elapsedSeconds to activities

--- src/models/activities.js
+++ src/models/activities.js
@@ -4,40 +4,48 @@ const activities = [{
   name: 'Fahrrad fahren',
   type: 'main',
   icon: 'bicycle',
+  elapsedSeconds: 1234,
 }, {
   id: '1',
   name: 'Arbeiten',
   type: 'main',
   icon: 'cash',
+  elapsedSeconds: 4567,
 }, {
   id: '2',
   name: 'Lernen',
   type: 'main',
   icon: 'school',
+  elapsedSeconds: 8796,
 }, {
   id: '3',
   name: 'Schlafen',
   type: 'main',
   icon: 'body',
+  elapsedSeconds: 6574,
 }, {
   id: '4',
   name: 'Essen',
   type: 'main',
   icon: 'restaurant',
+  elapsedSeconds: 6789,
 }, {
   id: '5',
   name: 'Fernsehen',
   type: 'main',
   icon: 'desktop',
+  elapsedSeconds: 9867,
 }, {
   id: '6',
   name: 'Sport',
   type: 'main',
   icon: 'american-football',
+  elapsedSeconds: 7777,
 }, {
   id: '7',
   name: 'Unterwegs',
   type: 'etc',
   icon: 'car',
+  elapsedSeconds: 8888,
 }];
 export default activities;
