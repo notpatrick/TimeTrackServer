9b71ff64dcba1ccbdb71bed263dc7dcde76e1c6c
Patrick <notpatrick@users.noreply.github.com>
2017-02-26 21:18:43 +0100

    Fix formatting

--- src/db.js
+++ src/db.js
@@ -1,16 +1,15 @@
 import mongoose from 'mongoose';
 import credentials from './credentials';
-import Activity from './models/activity.schema';
 
 export default (callback) => {
-	// connect to a database if needed, then pass it to `callback`:
-	mongoose.connect(`mongodb://${credentials.name}:${credentials.pw}@${credentials.url}`).then(
-		() => {
-			console.log('Connected to db');
-		},
-		(error) => {
-			console.log(error);
-		},
-	);
-	callback();
+  mongoose.Promise = global.Promise;
+  mongoose.connect(`mongodb://${credentials.name}:${credentials.pw}@${credentials.url}`).then(
+    () => {
+      console.log('Connected to db');
+    },
+    (error) => {
+      console.log(error);
+    },
+  );
+  callback();
 };
